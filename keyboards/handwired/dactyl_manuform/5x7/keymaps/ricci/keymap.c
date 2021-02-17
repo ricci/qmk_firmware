d18091f3799b08b9175798d40df51a61665cdc4f
Robert Ricci <ricci@cs.utah.edu>
2021-02-17 10:59:14 -0700

    Get rid of some stuff I don't use

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/ricci/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/ricci/keymap.c
@@ -7,13 +7,6 @@
 // entirely and just use numbers.
 #define _QWERTY 0
 #define _FN     1
-#define _NUMPAD 2
-
-// Some basic macros
-#define TASK    LCTL(LSFT(KC_ESC))
-#define TAB_R   LCTL(KC_TAB)
-#define TAB_L   LCTL(LSFT(KC_TAB))
-#define TAB_RO  LCTL(LSFT(KC_T))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -29,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
                      KC_PSCR, KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_MINS,
                      KC_INS,  KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,
-                 TG(_NUMPAD), KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+                     RESET,   KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
                               KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_EQL,
                                                 KC_LBRC,  KC_RBRC,  KC_HOME,  KC_END,
         KC_RALT, KC_SPC,  KC_ENT,
@@ -46,29 +39,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                _______, _______, _______,
         // right hand
                      _______,   KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,   _______,
-                     _______,   _______,   _______,   _______,   _______,   _______,   _______,
-                     _______,   KC_LEFT,   KC_DOWN,   KC_UP,     KC_RGHT,   _______,   _______,
-                                _______,   _______,   _______,   _______,   _______,   _______,
-                                                      _______,   _______,   _______,   _______,
+                     _______,   _______,   _______,   _______,   _______,   _______,  _______,
+                     _______,   KC_LEFT,   KC_DOWN,   KC_UP,     KC_RGHT,   _______,  _______,
+                                _______,   _______,   _______,   _______,   _______,  _______,
+                                                      _______,   _______,   _______,  _______,
         _______, _______, _______,
-        _______, _______, _______),
-
-[_NUMPAD] = LAYOUT_5x7_ALT(
-  // left hand
-   _______,   _______,   _______,   _______,   _______,   _______,  _______,
-   _______,   _______,   _______,   _______,   _______,   _______,  _______,
-   _______,   _______,   _______,   _______,   _______,   _______,  _______,
-   _______,   _______,   _______,   _______,   _______,   _______,
-   _______,   _______,   _______,   _______,
-                               _______, _______, _______,
-                               _______, _______, _______,
-        // right hand
-                     _______,   _______,   KC_NLCK,   _______,   KC_PMNS,   KC_PPLS,   _______,
-                     _______,   _______,   KC_P7,     KC_P8,     KC_P9,     _______,   _______,
-                     _______,   _______,   KC_P4,     KC_P5,     KC_P6,     KC_PAST,   _______,
-                                _______,   KC_P1,     KC_P2,     KC_P3,     KC_PSLS,   _______,
-                                                      KC_P0,     KC_PDOT,   _______,   _______,
-        _______, KC_PENT, _______,
-        _______, _______, _______),
+        _______, _______, _______)
 
 };
